02c479e5776e4439db561ac2fcc2eb7f35b160f4
Sohag-84 <ihsohag84@gmail.com>
2023-11-30 11:10:10 +0600

    send custom notification

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -15,6 +15,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descController = TextEditingController();
   NotificationServices notificationServices = NotificationServices();
 
   @override
@@ -36,46 +38,55 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Flutter Notifications'),
       ),
-      body: Center(
-        child: TextButton(
-            onPressed: () {
-              // send notification from one device to another
-              notificationServices.getToken().then((value) async {
-                var data = {
-                  "to":"/topics/allDevices",
-                  //'to': value.toString(),
-                  'notification': {
-                    'title': 'Ih Sohag',
-                    'body': 'Flutter developer',
-                    "sound": "jetsons_doorbell.mp3"
-                  },
-                  'android': {
+      body: Column(
+        children: [
+          TextField(
+            controller: titleController,
+          ),
+          SizedBox(height: 10),
+          TextField(
+            controller: descController,
+          ),
+          TextButton(
+              onPressed: () {
+                // send notification from one device to another
+                notificationServices.getToken().then((value) async {
+                  var data = {
+                    "to":"/topics/allDevices",
+                    //'to': value.toString(),
                     'notification': {
-                      'notification_count': 23,
+                      'title': titleController.text,
+                      'body': descController.text,
+                      "sound": "jetsons_doorbell.mp3"
                     },
-                  },
-                  'data': {'type': 'msg', 'id': 'Hello Flutter developer'}
-                };
+                    'android': {
+                      'notification': {
+                        'notification_count': 23,
+                      },
+                    },
+                    'data': {'type': 'msg', 'id': 'Hello Flutter developer'}
+                  };
 
-                await http.post(
-                    Uri.parse('https://fcm.googleapis.com/fcm/send'),
-                    body: jsonEncode(data),
-                    headers: {
-                      'Content-Type': 'application/json; charset=UTF-8',
-                      'Authorization':
-                          'key=AAAANh2HznU:APA91bGEn-YZ-3VDV85IuUsyKHN7fW4S2L7cn-tUx45SKKfZCfMTBwYbIOgBN23raNhxbViZX0NPrpvIAgl-XBPbYdohzQJona_e3C7Ww3XTTkdWwf5h_FB8HZLgHnCtyATu1wdFhUth'
-                    }).then((value) {
-                  if (kDebugMode) {
-                    print(value.body.toString());
-                  }
-                }).onError((error, stackTrace) {
-                  if (kDebugMode) {
-                    print(error);
-                  }
+                  await http.post(
+                      Uri.parse('https://fcm.googleapis.com/fcm/send'),
+                      body: jsonEncode(data),
+                      headers: {
+                        'Content-Type': 'application/json; charset=UTF-8',
+                        'Authorization':
+                            'key=AAAANh2HznU:APA91bGEn-YZ-3VDV85IuUsyKHN7fW4S2L7cn-tUx45SKKfZCfMTBwYbIOgBN23raNhxbViZX0NPrpvIAgl-XBPbYdohzQJona_e3C7Ww3XTTkdWwf5h_FB8HZLgHnCtyATu1wdFhUth'
+                      }).then((value) {
+                    if (kDebugMode) {
+                      print(value.body.toString());
+                    }
+                  }).onError((error, stackTrace) {
+                    if (kDebugMode) {
+                      print(error);
+                    }
+                  });
                 });
-              });
-            },
-            child: Text('Send Notifications')),
+              },
+              child: Text('Send Notifications')),
+        ],
       ),
     );
   }
